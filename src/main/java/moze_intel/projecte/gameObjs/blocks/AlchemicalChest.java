7d040a7cbede7369363d1918e67c99044d7d9b49
Thomas Pakh <thomas.pakh@gmail.com>
2014-12-01 10:59:24 -0500

    Changed unlocalized names back to the old ones

--- src/main/java/moze_intel/projecte/gameObjs/blocks/AlchemicalChest.java
+++ src/main/java/moze_intel/projecte/gameObjs/blocks/AlchemicalChest.java
@@ -23,7 +23,7 @@ public class AlchemicalChest extends BlockDirection implements ITileEntityProvid
 	public AlchemicalChest() 
 	{
 		super(Material.rock);
-		this.setBlockName("pe_alchemy_chest");
+		this.setBlockName("alchemy_chest");
 		this.setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.875F, 0.9375F);
 		this.setHardness(10.0f);
 	}
